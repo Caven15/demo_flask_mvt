16a607553589a68a341eac063a651f1f14e46b09
Caven15 <cavenditch15@gmail.com>
2025-05-28 11:26:56 +0200

    ajout sqlalchemy

---
+++ app/models/db/db_model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = 'task'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(200), nullable=False)
